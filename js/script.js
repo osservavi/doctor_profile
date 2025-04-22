ea2ced8a4538fc1b2a9a6c007b317019152f8a23
osservavi <dasavas70@gmail.com>
2025-04-22 18:37:31 +0300

    added progress bar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
-     // Плавная прокрутка для навигации
-     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+    // Плавная прокрутка для навигации
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
         });
     });
-    
-    // Фиксированная навигация при скролле
-    window.addEventListener('scroll', function() {
-        const nav = document.querySelector('nav');
-        if (window.scrollY > 100) {
-            nav.classList.add('shadow-sm');
-        } else {
-            nav.classList.remove('shadow-sm');
-        }
-    });
+});
+
+// Фиксированная навигация при скролле
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 100) {
+        nav.classList.add('shadow-sm');
+    } else {
+        nav.classList.remove('shadow-sm');
+    }
+});
+
+// progress bar 
+window.onscroll = function() {
+    updateProgressBar();
+    };
+
+    function updateProgressBar() {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+    }
